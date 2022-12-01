ba87979e18e51e4c50ba0d35c29ea248e6f4d628
armanceau <100920811+armanceau@users.noreply.github.com>
2022-12-01 09:45:24 +0100

    creation header + maj require php

--- header.php
+++ header.php
@@ -1,14 +1,16 @@
+<?php
+require "header.php";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <link href="style.css" rel="stylesheet">
 </head>
 <body>
-    <a href="connexion.php">Se connecter</a>
-    <a href="inscripton.php">S'inscrire</a>
-    <img src="logo.png">
+    
 </body>
 </html>
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+require "header.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <a href="connexion.php">Se connecter</a>
+    <a href="inscripton.php">S'inscrire</a>
+    <img src="logo.png">
+</body>
+</html>
