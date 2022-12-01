925098b6463b84235f3eb9191805f6e84bacd559
phprom2 <100920245+phprom2@users.noreply.github.com>
2022-12-01 11:59:06 +0100

    ajout choix des entrainements FONCTIONNELS

--- trainning.php
+++ trainning.php
@@ -15,6 +15,19 @@
             </option>
         <?php } ?>
     </select>
+    <br>
+    <label for="">Nom du champ </label>
+    <select name=""  id="" required>
+        <?php
+        $reponse = $connection->query("SELECT * FROM `echauffement`");
+        while ($donnees = $reponse->fetch())
+        {
+            ?>
+            <option value="categorie">
+                <?php echo $donnees['nom']; ?>
+            </option>
+        <?php } ?>
+    </select>
     <button type="submit">Choisis</button>
     </body>
 </html>
