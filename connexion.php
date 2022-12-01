e4e425ab57cf4d2173f5477922c22babe1d5f8b3
armanceau <100920811+armanceau@users.noreply.github.com>
2022-12-01 09:28:42 +0100

    update connexion.php and index.html

--- connexion.php
+++ connexion.php
@@ -4,23 +4,23 @@
     	<title>Mon formulaire</title>
     </head>
 
-
     <form method="GET" action=" on c pas " enctype="multipart/form-data">
-
-
-        <div>
-            <label for="name"> adresse email : </label><!--on créer un label donc un objet que lon peut reutiliser avec for il pointe sur l'id name   -->
-            <input id="name" name="name" type="text"/>
-        </div>
-
-        <div>
-            <label for="MDP"> mot de passe : </label><!--on créer un label donc un objet que lon peut reutiliser avec for il pointe sur l'id name   -->
-            <input id="MDP" name="MDP" type="text"/>
-        </div>
-
-        <div>
-            <button type="submit"> se connecter </input><!--autre forme de bouton-->
-        </div>
+        <fieldset>
+            <legend>Connexion</legend>
+            <div>
+                <label for="name">Adresse email : </label><!--on créer un label donc un objet que lon peut reutiliser avec for il pointe sur l'id name   -->
+                <input id="name" name="name" type="text"/>
+            </div>
+            <br>
+            <div>
+                <label for="MDP">Mot de passe : </label><!--on créer un label donc un objet que lon peut reutiliser avec for il pointe sur l'id name   -->
+                <input id="MDP" name="MDP" type="text"/>
+            </div>
+            <br>    
+            <div>
+                <button type="submit">Se connecter</input><!--autre forme de bouton-->
+            </div>
+        </fieldset>
     </form>
-
+    <a href="index.html"><button>Retour</button></a>
 </html>
