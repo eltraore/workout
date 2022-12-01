2b006c6ac138052205462099c889b659926c9d87
phprom2 <100920245+phprom2@users.noreply.github.com>
2022-12-01 10:52:41 +0100

    ajout choix des entrainements

---
+++ trainning.php
@@ -0,0 +1,19 @@
+<?php
+    include sqlconnect.php;
+?>
+<html>
+    <body>
+    <label for="">Nom du champ </label>
+    <select name=""  id="" required>
+        <?php
+        $reponse = $connection->query('SELECT * FROM "workout"');
+        while ($donnees = $reponse->fetch())
+        {
+            ?>
+            <option value="<?php echo $donnees['category']; ?>">
+            </option>
+        <?php } ?>
+    </select>
+    <button type="submit">Choisis</button>
+    </body>
+</html>
