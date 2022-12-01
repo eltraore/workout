97ce27ccd42422b10a4c6939e4d085a0a5928f7b
armanceau <100920811+armanceau@users.noreply.github.com>
2022-12-01 11:19:48 +0100

    Update trainning.php

--- trainning.php
+++ trainning.php
@@ -1,19 +1,20 @@
 <?php
-    include sqlconnect.php;
+    include "sqlconnect.php";
+    require "header.php";
 ?>
 <html>
     <body>
-    <label for="">Nom du champ </label>
+    <label for="">Choisir mon échauffement : </label>
     <select name=""  id="" required>
         <?php
         $reponse = $connection->query('SELECT * FROM "workout"');
         while ($donnees = $reponse->fetch())
         {
             ?>
-            <option value="<?php echo $donnees['category']; ?>">
+            <option value="<?php echo $donnees['categorie']; ?>">
             </option>
         <?php } ?>
     </select>
-    <button type="submit">Choisis</button>
+    <button type="submit">Choisir</button>
     </body>
 </html>
