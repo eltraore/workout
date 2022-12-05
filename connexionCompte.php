284dbe10595348e538e24dc9ece6c925e3817cb8
P3psHy <76128319+P3psHy@users.noreply.github.com>
2022-12-05 15:14:05 +0100

    Ajouts

---
+++ connexionCompte.php
@@ -0,0 +1,38 @@
+<?php
+
+try{
+
+    require "sqlconnect.php" ;
+   
+    $sql= $connection->prepare("SELECT mail,MDP FROM employer WHERE mail = :mail AND MDP = :MDP") ;
+
+    $sql->bindParam(':mail', $_REQUEST["mail"], PDO::PARAM_STR);
+    $sql->bindParam(':MDP', SHA1($_REQUEST['password']), PDO::PARAM_STR);
+
+  
+    $sql->execute();
+    $ligne = $sql->fetchall();
+
+    if(!empty($ligne))
+    {
+        if ($_REQUEST["mail"]==$ligne[0]['mail'] && SHA1($_REQUEST['password'])==$ligne[0]['MDP'] ) {  
+
+            //$_SESSION['id'] = $id;
+            //$_SESSION['mdp'] = $mdp;
+            //$_SESSION['erreurConnect']=false;
+            header("Location: trainning.php");
+        
+    }
+    }else{
+
+        //$_SESSION['erreurConnect']=true;
+        header("Location: index.php");
+        
+        }
+
+}catch (PDOException $e){
+    echo "Erreur: ".$e->getMessage();
+    echo"<a href =\"index.php\">Retour à l'accueil</a>";
+}
+
+?>
