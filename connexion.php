67460f8013603b5871369a7bf2a0760565413c25
armanceau <100920811+armanceau@users.noreply.github.com>
2022-12-08 11:57:56 +0100

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -7,37 +7,63 @@ require "header.php";
     <script src="Connexion.js"></script>
 </head>
 
-    
-    <form method="GET" action="connexionCompte.php">
-        <fieldset>
-            <legend>Connexion</legend>
+    <body>
+        <form method="GET" action="connexionCompte.php" style="margin-top: 100px;">
+            <div class="row">
+                <div class="col-4"> </div>
+                    <div class="col-4" style="text-align: center;">
+                    <a href="accueil.php"><img src="logo.png" height="80" width="80"></a> </br></br>
+                    </div>
+                <div class="col-4"> </div>
+            </div>
+
+            <div class="row" style="color: white;">
+                <div class="col-2"></div>
+                <div class="col-8" style="text-align: center;">
+                        <label for="mail">Adresse email : </label><!--on créer un label donc un objet que lon peut reutiliser avec for il pointe sur l'id name   -->
+                        <input name="mail" type="text"  id="Mail" onfocusout="verifMail()"/>
+                        <div id="erreurMail"></div>   
+                </div>
+                <div class="col-2"></div>
+            </div>
+
+            <br>
+
+            <div class="row" style="color: white;">
+                <div class="col-2"></div>
+                <div class="col-8" style="text-align: center;">
+                    <label for="password">Mot de passe : </label><!--on créer un label donc un objet que lon peut reutiliser avec for il pointe sur l'id name   -->
+                    <input id="password" name="password" type="password"/><div id="erreurMail"></div>   
+                </div>
+                <div class="col-2"></div>
+            </div>
+            <br>
             <?php 
             session_start();
             
 
             if(!isset($_SESSION['erreurConnect']) || $_SESSION['erreurConnect'])
             {
-                echo "<p> Login ou mot de passe incorect </p>" ;
+                echo "<p style=\"color: white; text-align:center;\">Adresse mail ou mot de passe incorrect !</p>" ;
             }
-            
-            
-            ?>
-            <div>
-                <label for="mail">Adresse email : </label><!--on créer un label donc un objet que lon peut reutiliser avec for il pointe sur l'id name   -->
-                <input name="mail" type="text"  id="Mail" onfocusout="verifMail()"/>
-                <div id="erreurMail"></div>   
-            </div>
+            ?>  
+
             <br>
-            <div>
-                <label for="password">Mot de passe : </label><!--on créer un label donc un objet que lon peut reutiliser avec for il pointe sur l'id name   -->
-                <input id="password" name="password" type="password"/>
-            </div>
-            <br>    
-            <div>
-                <button type="reset">Annuler</input><!--autre forme de bouton-->
-                <button type="submit">Se connecter</input><!--autre forme de bouton-->      
-            </div>
-        </fieldset>
-    </form>
-    <a href="accueil.php"><button>Retour</button></a>
+            
+            <div class="row">
+                <div class="col-1"></div>
+                <div class="col-9" style="text-align: center;">
+                    <div class="row">
+                        <div class="col-6" style="text-align: center;"> 
+                            <button type="reset" style="color: white;" class="btn btn-dark rounded-pill btn-sm">Annuler</button>
+                        </div>
+                        <div class="col-6" style="text-align: center;"> 
+                            <button type="submmit" style="color: white; text-align: center;" class="btn btn-dark rounded-pill btn-sm">Se connecter</button>
+                        </div>
+                    </div> 
+                </div>
+                <div class="col-1"></div>
+            </div>          
+        </form>
+    </body>
 </html>
