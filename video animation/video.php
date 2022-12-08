975600e72ab23aceefbd0ed59d40719ca19a8528
natzan <70909030+natzan@users.noreply.github.com>
2022-12-08 12:41:41 +0100

    debut animaton

---
+++ video animation/video.php
@@ -0,0 +1,16 @@
+<html>
+    <body>
+        <section class="hero">
+            <video loop="loop" autoplay="autoplay" muted>
+                <source src="" type="video/mp4">
+            </video>
+
+            <div class="content">
+                <h2>Workout</h2>
+                <p> HELLo
+                <a href="">CLIK LA</a>
+
+            </div>
+        </section>
+    </body>
+</html>
