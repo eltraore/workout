035eef1c7fbc6aafcef0afc5e3191c87d43ace49
armanceau <100920811+armanceau@users.noreply.github.com>
2022-12-08 08:25:23 +0100

    création detail_activite+maj css

---
+++ Detail_activite.php
@@ -0,0 +1,46 @@
+<?php
+require "header.php";
+?>
+
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <script src="https://kit.fontawesome.com/2d470788f0.js" crossorigin="anonymous"></script>
+    <title>Détail activité</title>
+</head>
+<body>
+<header>
+    
+<span class="material-symbols-outlined">
+
+</span>
+    <button class="button_logout">
+
+
+</header>
+
+<main>
+    <div class="bloc">
+        <center>
+
+            <img src="logo.png" height="100" width="100"> </br></br>
+            <video width="200" height="350" controls autoplay>
+                <source src=”https://www.youtube.com/watch?v=NvIGKRMNnSk” type=video/ogg> <source src="/build/videos/arcnet.io(7-sec).mp4" type=video/mp4>
+            </video>
+        </center>
+    </div>
+    <center>
+        <button class="button_finish">
+            J'ai finis!
+        </button>
+    </center>
+</main>
+</body>
+</html>
+
