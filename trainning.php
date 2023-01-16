27738ea61669f964f64eb2783493f85629f34f36
armanceau <100920811+armanceau@users.noreply.github.com>
2023-01-16 19:34:06 +0100

    update trainning.php

--- trainning.php
+++ trainning.php
@@ -4,21 +4,70 @@
     <title>Workout | Catégories</title>
 </head>
 <body>
-    <?php
-    include "sqlconnect.php";
 
-    $sql= $connection->prepare("SELECT * FROM categorie") ; 
-    $sql->execute();
-    $ligne = $sql->fetchall();
+<div class="container">
 
-    foreach($ligne as $categorie){
-        echo "<a href=\"echauffement.php?id=".$categorie['id']."\">
-        <div>
-            <h3>".$categorie['nom']."</h3>
+    <br>
+    <div class="row">
+        <div class="col-4"></div>
+            <div class="col-4" style="text-align: center;">
+                <a href="accueil.php"><img src="logo.png" height="80" width="80"></a> </br></br>
+            </div>
+        <div class="col-4"></div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-1"></div>
+            <div class="col-10 case coin-arrondi">
+            <?php
+                include "sqlconnect.php";
+
+                $sql= $connection->prepare("SELECT * FROM employer") ; 
+                $sql->execute();
+                $ligne = $sql->fetchall();
+
+                foreach($ligne as $employer){
+
+                    echo "<h3 class=\"text\"> ".$employer['prenom']." ".$employer['nom']."</h3>";
+                    
+                }
+            ?>
+            </div>
+            <div class="col-1"></div>
         </div>
-        </a>";
-    }
-    ?>
+    </div>
+    
+    <br>
+    <br>
+    <div class="container">
+        <div class="row">
+        <?php
+            include "sqlconnect.php";
+
+            $sql= $connection->prepare("SELECT * FROM categorie") ; 
+            $sql->execute();
+            $ligne = $sql->fetchall();
+
+            foreach($ligne as $categorie){
+                echo'
+                    <div class="col-2"></div>
+                    <div class="col-8 categorie coin-arrondi">
+                        <a class="lien" href="echauffement.php?id="'.$categorie['id'].'">
+                            <img src="'.$categorie["categ_icons"].'" height="50px"/ width="50px">
+                            <h3 class="nom_categ">'.$categorie['nom'].'</h3>
+                        </a>
+                    </div>  
+                    <div class="col-2"></div>
+
+                ';  
+            }
+        ?>
+        </div>
+    </div>
+    
+</div>
+    
 </body>
 </html>
 
