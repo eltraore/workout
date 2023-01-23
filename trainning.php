c1afae0a56e05cf3733d2881aba008545812ca7c
armanceau <100920811+armanceau@users.noreply.github.com>
2023-01-23 20:08:41 +0100

    update css training.php

--- trainning.php
+++ trainning.php
@@ -56,7 +56,7 @@
                         <div class="col-2"></div>
                         <div class="col-8 categorie coin-arrondi">
                             <a class="lien" href="echauffement.php?id="'.$categorie['id'].'">
-                                <img src="'.$categorie["categ_icons"].'" height="50px"/ width="50px">
+                                <img class=" bounce2" src="'.$categorie["categ_icons"].'" height="50px"/ width="50px">
                                 <h3 class="nom_categ">'.$categorie['nom'].'</h3>
                             </a>
                         </div>  
