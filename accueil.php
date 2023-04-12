9f02144dddfbb738f39bd11a6313e93c17e6969e
Elijah <100931397+eltraore@users.noreply.github.com>
2023-04-12 16:40:04 +0200

    a

--- accueil.php
+++ accueil.php
@@ -6,37 +6,28 @@ require "header.php";
     <title>Workout | Accueil </title>
 </head>
 <body>
-    <div class="container">
-        <div class="row">
-            <div class="col-2"> </div>
-                <div class="col-8" style="text-align: center; margin-top: 150px;">
-                    <img src="assets\logo.png" height="200" width="200"> </br></br>
+    <div class=bg-top></div>
+
+    <div class="accueil-wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col text-center">
+                    <img src="assets\logo.png" height="280" width="280">
                 </div>
-            <div class="col-2"> </div>
-        </div>
-        
-
-
-        <div class="row">
-            <div class="col-2"> 
-                
             </div>
 
-            <div class="col-8"> 
-                <div class="row">
-                    <div class="col-6" style="text-align: center;">
-                        <button type="button" class="btn btn-dark rounded-pill"><a style="color: white; text-decoration: none;" href="connexion.php">Connexion</a></button>
-                    </div>
+            <div class="row m-2">
+                <div class="col-6">
+                    <div type="button" class="btn-accueil"><a href="connexion.php">Connexion</a></div>
+                </div>
 
-                    <div class="col-6" style="text-align: center;">
-                        <button type="button"  class="btn btn-dark rounded-pill"><a style="color: white; text-decoration: none;" href="Inscription.php">Inscription</a></button>
-                    </div>
-                </div>  
+                <div class="col-6">
+                    <div type="button" class="btn-accueil"><a href="Inscription.php">Inscription</a></div>
+                </div>
             </div>
-                
-            <div class="col-2"></div>
         </div>
     </div>
+    
     
 </body>
 </html>
