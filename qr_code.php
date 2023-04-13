0c7d8cab6fc6c7588f662dc99c3e3cb7b19785c8
Elijah <100931397+eltraore@users.noreply.github.com>
2023-04-13 16:10:35 +0200

    7

--- qr_code.php
+++ qr_code.php
@@ -1,18 +1,94 @@
 <?php
-require "header.php";
-include "sqlconnect.php";
-$sql= $connection->query("SELECT CURRENT_USER;") ;
-$sql->execute();
-$ligne = $sql->fetchall();
+    require "header.php";
+    include "sqlconnect.php";
+    $sql= $connection->query("SELECT CURRENT_USER;") ;
+    $sql->execute();
+    $ligne = $sql->fetchall();
 
-$user = $ligne[0][0];
+    $user = $ligne[0][0];
 
-preg_match('/(.*)@/', $user, $output_array);
+    $sql= $connection->prepare("SELECT *
+                                FROM employer
+                                ") ;
+    $sql->bindParam(':id', $_REQUEST['id']);
+    $sql->execute();
+    $ligne = $sql->fetchall();
+
+    foreach($ligne){
+        $nom = $employer['nom'];
+    }
+
+    preg_match('/(.*)@/', $user, $output_array);
 ?>
 <html>
 
     <body>
-        <div class="container">
+
+    <div class="training-container">
+        <div class="top-container">
+            <div>
+                <a href="#" onclick="history.back()"><svg width="50px" height="50px" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg" stroke=""><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M12.9998 8L6 14L12.9998 21" stroke="#ffffff" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M6 14H28.9938C35.8768 14 41.7221 19.6204 41.9904 26.5C42.2739 33.7696 36.2671 40 28.9938 40H11.9984" stroke="#ffffff" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg></a>
+            </div>
+            <div class="img-top">
+                <a href="accueil.php">
+                    <img src="assets\logo.png" height="120" width="120">
+                </a>
+            </div>
+        </div>
+
+        <div class="training-wrapper">
+            <div class="training-card">
+                <div class="text-default mb-3">
+                    Félicitations
+                    <span class="red-text">
+                        <?php
+                            echo "".$ligne['prenom']."";
+                        ?>
+                    </span>
+                    !<br>
+                    Tu es maintenant bien échauffé,
+                    <br>
+                    Voici ta récompense
+                </div>
+            </div>
+        </div>
+
+        <div class="video-container">
+            <div class="qr-wrapper text-center">
+                <img class="qr-code" src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=
+                <?php
+                    echo $output_array[1]."/".time();
+                ?>" width="100%">
+            </div>
+        </div>
+
+        <div class="mb-80 text-center">
+            <form action="qr_code.php">
+                <button type="submit" class="btn-wrapper">J’ai finis !</button>
+            </form>
+        </div>
+
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+        <!-- <div class="container">
 
             <?php require "nav.php";?>
 
@@ -52,7 +128,7 @@ preg_match('/(.*)@/', $user, $output_array);
             </div>
 
 
-        </div>
+        </div> -->
 
 
 
